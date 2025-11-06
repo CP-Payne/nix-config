af52654df1f46cdcd806513064df026b786ed287
Charles Payne <charlethical1@gmail.com>
2025-11-06 18:09:39 +0200

    Cleaned base

--- modules/system/user.nix
+++ modules/system/user.nix
@@ -4,7 +4,7 @@
 	# Don't forget to set password with 'passwd'.
 	users.users.${vars.username} = {
 		isNormalUser = true;
-		extraGroups = [ "Wheel" "networkmanager" ];
+		extraGroups = [ "wheel" "networkmanager" ];
 		description = vars.fullName;
 		shell = pkgs.zsh;
 	};
