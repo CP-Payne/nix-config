05add2a0f1b0d399c68b28086e07b1e5220aae28
Charles Payne <charlethical1@gmail.com>
2025-11-07 08:59:58 +0200

    Changed global font default to JetBrainsMono

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -11,15 +11,21 @@
   #     cascadia-code
   #   ];
 
- fonts.packages = [
-	pkgs.nerd-fonts.jetbrains-mono
-	pkgs.nerd-fonts.fira-code
-	pkgs.nerd-fonts.iosevka
-	pkgs.nerd-fonts.iosevka-term
- ];
+	fonts = {
 
-fonts.fontconfig.defaultFonts = {
-      monospace = [ "JetBrainsMono Nerd Font" ];
-      sansSerif = [ "FiraCode Nerd Font" ];
-    };
+		packages = [
+			pkgs.nerd-fonts.jetbrains-mono
+			pkgs.nerd-fonts.fira-code
+			pkgs.nerd-fonts.iosevka
+			pkgs.nerd-fonts.iosevka-term
+		];
+
+	 
+		fontconfig.defaultFonts = {
+			monospace = [ "JetBrainsMono Nerd Font" ];
+			sansSerif = [ "JetBrainsMono Nerd Font" ];
+			serif = [ "JetBrainsMono Nerd Font" ];
+		};
+	};
+
 }
