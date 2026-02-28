173a31655706801847bcbaaf3bb3e9217a2845a1
Charles Payne <64592568+CP-Payne@users.noreply.github.com>
2026-02-28 07:39:49 +0000

    Start VBoxClient in XFCE sessions on VirtualBox guests

--- desktop/xfce.nix
+++ desktop/xfce.nix
@@ -6,17 +6,20 @@
 }: let
   cfg = config.desktop.xfce;
   isSelected = config.desktop.session.type == "xfce";
+  xfceSession = "${pkgs.xfce.xfce4-session}/bin/startxfce4";
+  xfceSessionWithVBoxClient = "${pkgs.bash}/bin/bash -lc '${pkgs.virtualboxGuestAdditions}/bin/VBoxClient-all & exec ${xfceSession}'";
 in {
   options.desktop.xfce.enable = lib.mkEnableOption "Enable XFCE desktop";
 
   config = lib.mkIf (cfg.enable || isSelected) {
     services.xserver.enable = true;
+    services.xserver.libinput.enable = true;
     services.xserver.desktopManager.xfce.enable = true;
 
     environment.systemPackages = [
       pkgs.xorg.xinit
     ];
 
-    desktop.session.cmd = lib.mkDefault "${pkgs.dbus}/bin/dbus-run-session ${pkgs.xfce.xfce4-session}/bin/startxfce4";
+    desktop.session.cmd = lib.mkDefault "${pkgs.dbus}/bin/dbus-run-session ${if config.virtualisation.virtualbox.guest.enable then xfceSessionWithVBoxClient else xfceSession}";
   };
 }
